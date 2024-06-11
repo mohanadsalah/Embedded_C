395a05988b2c1dadd4f682427a0ce23214339359
mohanadsalah <139906751+mohanadsalah@users.noreply.github.com>
2024-06-11 10:38:43 +0300

    Update app.c

--- Embedded_C/lab2/app.c
+++ Embedded_C/lab2/app.c
@@ -1,6 +1,6 @@
 #include "uart.h"
-unsigned char string_buffer[100]="learn-in-depth:Mostafa-Elshiekh";
-unsigned char const string_buffer2[100]="learn-in-depth:Mostafa-Elshiekh";
+unsigned char string_buffer[100]="learn-in-depth:Mohanad_Mohamed";
+unsigned char const string_buffer2[100]="learn-in-depth:Mohanad_Mohamed";
 
 
 void main(void)
