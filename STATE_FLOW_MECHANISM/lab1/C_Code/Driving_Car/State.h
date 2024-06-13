6431596b211e4ffc4ea72dd8aec48aff4cf37eb4
mohanadsalah <139906751+mohanadsalah@users.noreply.github.com>
2024-06-13 17:24:54 +0300

    Delete STATE_FLOW_MECHANISM directory

--- STATE_FLOW_MECHANISM/lab1/C_Code/Driving_Car/State.h
+++
@@ -1,8 +0,0 @@
-#ifndef STATE_H_
-#define STATE_H_
-
-// Automatic State Function Generated
-#define State_define(_statFUN_) void ST_##_statFUN_()
-#define State(_statFUN_) ST_##_statFUN_
-
-#endif /* STATE_H_ */
